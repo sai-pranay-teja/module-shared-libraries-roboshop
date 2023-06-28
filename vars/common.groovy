653249112375921a0937263431a4d264751ed8e1
saipranayteja <pranayteja.ch@algonox.com>
2023-06-28 23:55:14 +0530

    sdcf

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,6 +8,10 @@ def complie(){
     }
 }
 
+// def codequality(){
+//     sh 'sudo sonar-scanner -Dsonar.host.url=http://172.31.87.135:9000 -Dsonar.login=admin -Dsonar.password=roboshop -Dsonar.projectKey=$(component)'
+// }
+
 def codequality(){
-    sh 'sudo sonar-scanner -Dsonar.host.url=http://172.31.87.135:9000 -Dsonar.login=admin -Dsonar.password=roboshop -Dsonar.projectKey=$(component)'
+    sh 'sudo sonar-scanner -Dsonar.host.url=http://44.197.195.98:9000 -Dsonar.login=admin -Dsonar.password=roboshop -Dsonar.projectKey=$(component)'
 }
