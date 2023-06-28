61930048bf0ddd6bcd4b71d92668383ebd6398aa
saipranayteja <pranayteja.ch@algonox.com>
2023-06-28 12:52:46 +0530

    szxdcf

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,9 +1,9 @@
 def complie(){
-    if app_lang=="nodejs" {
+    if (app_lang=="nodejs") {
         sh 'npm install'
     }
 
-    if app_lang=="maven" {
+    if (app_lang=="maven") {
         sh 'mvn package'
     }
 }
