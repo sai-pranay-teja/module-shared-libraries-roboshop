219aca9e697c682d276bb1ed3c283f88a6ec0d35
saipranayteja <pranayteja.ch@algonox.com>
2023-06-29 12:55:11 +0530

    sdfcv

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -22,7 +22,7 @@ pipeline {
     }
     post{
         failure{
-            mail body: "${component} pipeline - has failed /n ${BUILD_URL}", from: 'algonox1.1@gmail.com', subject: "${component} pipeline - has failed", to: "algonox1.1@gmail.com"
+            mail body: "${component} pipeline - has failed \n ${BUILD_URL}", from: 'algonox1.1@gmail.com', subject: "${component} pipeline - has failed", to: "algonox1.1@gmail.com"
     }
 
 }
