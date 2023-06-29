c6d498c7a169a4929ad373dbe5e35114ed83c200
saipranayteja <pranayteja.ch@algonox.com>
2023-06-29 12:49:21 +0530

    sdcfv

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -20,12 +20,11 @@ pipeline {
             }
         }
     }
-}
-
-post{
-    failure{
-        mail body: '${component} pipeline - has failed /n ${BUILD_URL}', from: 'algonox1.1@gmail.com', subject: '${component} pipeline - has failed', to: 'algonox1.1@gmail.com'
+    post{
+        failure{
+            mail body: '${component} pipeline - has failed /n ${BUILD_URL}', from: 'algonox1.1@gmail.com', subject: '${component} pipeline - has failed', to: 'algonox1.1@gmail.com'
     }
 
+}
 }
 }
