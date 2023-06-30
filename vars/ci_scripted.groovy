eb26ee9b8bfe5843e9202b6837c5d2135339ca70
saipranayteja <pranayteja.ch@algonox.com>
2023-06-30 15:28:37 +0530

    asxdcfvg

---
+++ vars/ci_scripted.groovy
@@ -0,0 +1,37 @@
+def call(){
+    if (!env.sonar_opts) {
+        sonar_opts=""
+    } 
+
+node {
+        
+        stage('Check out the repo'){
+            ls -l
+            cleanWs()
+            ls -l
+            git branch: 'main', url: 'https://github.com/sai-pranay-teja/cart'
+            ls -l
+
+            
+        }
+
+        stage('Build/Execute') {
+            common.complie()
+        }
+
+        stage('Test Cases') {
+            common.testcases()
+        }
+
+        stage('Code Quality') {
+            common.codequality()
+        }
+//     post{
+//         failure{
+//             mail body: "${component} pipeline - has failed \n ${BUILD_URL}", from: 'algonox1.1@gmail.com', subject: "${component} pipeline - has failed", to: "algonox1.1@gmail.com"
+//     }
+
+// }
+}
+}
+
