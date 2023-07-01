2bcffaffdfff8ce3959222b1813e9b0f14387131
saipranayteja <pranayteja.ch@algonox.com>
2023-07-01 14:40:29 +0530

    dcfv

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -16,7 +16,7 @@ node {
             sh 'ls -l'
             cleanWs()
             sh 'ls -l'
-            git branch: 'main', url: 'https://github.com/sai-pranay-teja/${component}'
+            git branch: 'main', url: "https://github.com/sai-pranay-teja/${component}"
             sh 'ls -l'
 
             sh 'env'
