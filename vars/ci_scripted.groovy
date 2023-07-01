b6853ab01eb70694f2ef581c7e0897200b01e861
saipranayteja <pranayteja.ch@algonox.com>
2023-07-01 12:32:52 +0530

    SXDCF

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -37,9 +37,14 @@ node {
         }
         }
 
-        stage('Code Quality') {
-            common.codequality()
+        if (env.BRANCH_NAME==~"PR.*"){
+            stage('Code Quality') {
+                common.codequality()
         }
+
+        }
+
+
 //     post{
 //         failure{
 //             mail body: "${component} pipeline - has failed \n ${BUILD_URL}", from: 'algonox1.1@gmail.com', subject: "${component} pipeline - has failed", to: "algonox1.1@gmail.com"
