6cb88405a16ee5be34341b67ae867a382402610b
saipranayteja <pranayteja.ch@algonox.com>
2023-07-04 17:34:30 +0530

    sxdcfv

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -10,14 +10,27 @@ pipeline {
     parameters {
         string(name: 'ENV', defaultValue: 'dev', description: 'Choose the Environment')
         string(name: 'ACTION', defaultValue: '', description: 'Choose the Action')
+        string(name: 'CLEAN', defaultValue: '', description: 'Choose the cleaning action')
     }
     stages {
-
-
-        stage('init') {
-            steps {
-                sh 'terraform init -backend-config env-${ENV}/state.tfvars'
+        if(CLEAN=="no")
+        {
+            stage('init') {
+                steps {
+                    sh 'terraform init -backend-config env-${ENV}/state.tfvars'
             }
+        }
+        }
+
+        if(CLEAN=="yes")
+        {
+            stage('init') {
+                steps {
+                    cleanWs()
+                    sh 'terraform init -backend-config env-${ENV}/state.tfvars'
+            }
+        }
+
         }
 
         stage('apply/destroy') {
