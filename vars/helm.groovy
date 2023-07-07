65a6d93d1bd8ad2824cff4173e822e126f9a754a
saipranayteja <pranayteja.ch@algonox.com>
2023-07-07 16:24:11 +0530

    sdcf

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -29,7 +29,7 @@ pipeline {
 
         stage('Server Deployment') {
             steps {
-                sh 'helm install ${COMPONENT} . -f APP/helm/${ENV}.yaml --set appversion=${APP_VERSION}'
+                sh 'helm upgrade -i ${COMPONENT} . -f APP/helm/${ENV}.yaml --set appversion=${APP_VERSION}'
 
             }
 
