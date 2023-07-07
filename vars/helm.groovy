061097b5dd753280457f38ca05001d71a8a5286f
saipranayteja <pranayteja.ch@algonox.com>
2023-07-07 13:27:33 +0530

    zsxdcv

---
+++ vars/helm.groovy
@@ -0,0 +1,48 @@
+def call() {
+pipeline {
+    agent any
+
+    options {
+        ansiColor('xterm')
+    }
+
+    
+    parameters {
+        string(name: 'APP_VERSION', defaultValue: '', description: 'Choose the App Version')
+        string(name: 'COMPONENT', defaultValue: '', description: 'Choose the Component')
+        string(name: 'ENV', defaultValue: '', description: 'Choose the Environment')
+    }
+    stages {
+        // stage('Ansible installation'){
+        //     steps{
+        //         sh 'sudo labauto ansible'
+        //     }
+        // }
+        stage('Clone Application') {
+            steps {
+                dir('APP'){
+                    git branch: 'main', url: "https://github.com/sai-pranay-teja/${component}"
+                }
+
+            }
+        }
+
+        stage('Server Deployment') {
+            steps {
+
+            }
+
+        }
+    }
+
+    post{
+        always{
+            cleanWs()
+    }
+
+}
+
+}
+
+
+}
